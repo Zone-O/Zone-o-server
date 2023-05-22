e36e0c4fa23446980a30ae474ec6c4c29958d4e0
osvegn <thomas.prudhomme@epitech.eu>
2023-05-22 10:45:19 +0200

    Fix coding style in tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,13 +1,14 @@
-const request = require('supertest');
-const app = require('../sources/index.js');
+const request = require('supertest')
+const app = require('../sources/index.js')
+const { describe, it, afterAll, expect } = require('@jest/globals')
 
-describe("GET /", () => {
-    it("should return 200 OK", async () => {
-        const response = await request(app).get("/");
-        expect(response.statusCode).toBe(200);
-    });
-});
+describe('GET /', () => {
+  it('should return 200 OK', async () => {
+    const response = await request(app).get('/')
+    expect(response.statusCode).toBe(200)
+  })
+})
 
 afterAll(() => {
-    app.close();
-});
+  app.close()
+})
